64de5e1b8e295cacfc64f1cbab0633312b84f96e
Orgun <100226060+OrgunTheExplorer@users.noreply.github.com>
2025-10-24 16:42:57 +0300

    Update Determinant of matrix.m

--- Determinant of matrix.m
+++ Determinant of matrix.m
@@ -1,13 +1,12 @@
-
 syms Kr Kz Bo Bz p w k R Z real
 syms Omega(R)          
 syms P(R,Z)            
-i = sym('i');        
+i = sym('1i');        
 
 
 a = [Kr, 0, Kz, 0, 0, 0, 0;
     (-i*w), -2*Omega, 0, (i*Kr)/(p), (-i*Kz*Bz)/(4*pi*p), (i*Kr*Bo)/(4*pi*p), (i*Kr*Bz)/(4*pi*p);
-    (k^2)/(2*Omega), (-i*w), 0, 0, 0, (-i*Kz*Bz)/(4*pi*p), 0;
+    (diff(Omega*R^2,R)/R), (-i*w), 0, 0, 0, (-i*Kz*Bz)/(4*pi*p), 0;
     0, 0, (-i*w), (i*Kz)/(p), 0, (i*Kz*Bo)/(4*pi*p), 0;
     (-i*Kz*Bz), 0, 0, 0, (-i*w), 0, 0;
     0, 0, (-i*Kz*Bz), 0, 0, 0, (-i*w);
@@ -28,9 +27,3 @@ annotation('textbox',[0 0 1 1], ...
     'EdgeColor','none');
 
 axis off
-
-
-
-
-
-
